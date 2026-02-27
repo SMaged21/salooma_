9c267734a9b3ad9bee3f94a8224c1c037c99acdf
salma <zweensam@gmail.com>
2026-02-27 22:24:01 +0200

    splash animation and splash view done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:salooma_app/core/assets/app_images.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> fadeAnimation;
+  late AnimationController fadeController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    fadeController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    fadeAnimation = Tween<double>(begin: 0, end: 1).animate(fadeController);
+
+    fadeController.forward(); // start animation
+  }
+
+  @override
+  void dispose() {
+    fadeController.dispose(); // correct dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image(image: AssetImage(AppImages.logo))],
+      children: [
+        FadeTransition(
+          opacity: fadeAnimation,
+          child: Image.asset(AppImages.logo),
+        ),
+      ],
     );
   }
 }
